535ee2d3104555fe712cf187c68aac7434f36600
DARKVENT ( Lokesh Kumar ) <lokesh.suman911@gmail.com>
2020-12-18 10:54:28 +0530

    version change and changelog update

--- school-management.php
+++ school-management.php
@@ -3,7 +3,7 @@
  * Plugin Name: The School Management
  * Plugin URI: https://weblizar.com/plugins/school-management/
  * Description: The School Management is a WordPress plugin to manage multiple schools and their entities such as classes, sections, students, exams, ID cards, admit cards, teachers, staff, fees, invoices, income, expense, noticeboard, study materials and much more.
- * Version: 6.7
+ * Version: 6.8
  * Author: Weblizar
  * Author URI: https://weblizar.com
  * Text Domain: school-management
